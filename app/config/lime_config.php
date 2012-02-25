7f7552751337cde1730ed1f3669343d0f2fa40e5
Tac Tacelosky <tacman@gmail.com>
2012-02-25 17:53:57 -0500

    add limesurvey and photos

---
+++ app/config/lime_config.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * LimeSurvey
+ * Copyright (C) 2007 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or is
+ * derivative of works licensed under the GNU General Public License or other
+ * free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ *
+ * $Id$
+ */
+
+/* IMPORTANT NOTICE
+ *  With LimeSurvey v1.70+ the configuration of LimeSurvey was simplified,
+ *  Now config.php only contains the basic required settings.
+ *  Some optional settings are also set by default in config-defaults.php.
+ *  If you want to change an optional parameter, DON'T change values in config-defaults.php!!!
+ *  Just copy the parameter into your config.php-file and adjust the value!
+ *  All settings in config.php overwrite the default values from config-defaults.php
+ */
+
+// Basic Setup
+
+$databasetype       =   'mysqli';      // ADOdb database driver - valid values are mysql, mysqli, odbc_mssql, mssql_n, odbtp or postgres
+                                       // mysql: Recommended driver for mysql
+                                       // mysqli: Slightly faster driver for mysql - not on all server systems available
+                                       // odbc_mssql: MSSQL driver using ODBC with MS SQL Server
+                                       // mssqlnative: Native SQL Server driver for SQL Server 2005+
+                                       // mssql_n: Experimental driver for MS SQL Server which handles UTF-8 charsets
+                                       // odbtp: ODBTP driver to access MSSQL-Server is needed for this one - we also recommend to activate $databasepersistent for decent speed
+                                       // postgres: Standard postgres driver
+
+#$databaselocation   =   'rack3.smokescreen.org';   // Network location of your Database - for odbc_mssql or mssqlnative use the mssql servername, not localhost or IP
+$databaselocation   =   'localhost';   // Network location of your Database - for odbc_mssql or mssqlnative use the mssql servername, not localhost or IP
+$databasename       =   'db_lime_wedding';  // The name of the database that we will create
+$databaseuser       =   'lime';        // The name of a user with rights to create db (or if db already exists, then rights within that db)
+$databasepass       =   'l1m3';            // Password of db user
+/*
+$databaselocation   =   'cari1.smokescreen.org';   // Network location of your Database - for odbc_mssql or mssqlnative use the mssql servername, not localhost or IP
+$databasename       =   'db_lime3';  // The name of the database that we will create
+$databaseuser       =   'lime';        // The name of a user with rights to create db (or if db already exists, then rights within that db)
+$databasepass       =   'l1m3';            // Password of db user
+*/
+$supportedLanguagesKeys = 'en';
+
+$dbprefix           =   '';       // A global prefix that can be added to all LimeSurvey tables. Use this if you are sharing
+// a database with other applications. Suggested prefix is 'lime_'
+
+// File Locations
+$rooturl            =   "http://{$_SERVER['HTTP_HOST']}/rsvp"; // The root web url for your limesurvey installation (without a trailing slash).
+// The double quotes (") are important.
+
+// $rootdir            =   dirname(__FILE__); // This is the physical disk location for your limesurvey installation. Normally you don't have to touch this
+// setting. If you use IIS then you MUST enter the complete rootdir e.g. : $rootDir='C:\Inetpub\wwwroot\limesurvey'!
+// Some IIS and OS/2 installations also require to use forward slashes
+// instead of backslashes, e.g.  $rootDir='C:/Inetpub/wwwroot/limesurvey'!
+
+// Installation Setup
+$defaultuser        =   'admin';           // This is the username when LimeSurvey is installed and the administration user is created on installation
+$defaultpass        =   'nosmoke';        // This is the password for the administration user when LimeSurvey is installed
+
+// Debug Settings
+$debug              =   3;                 // Set this to 1 if you are looking for errors. If you still get no errors after enabling this
+                                           // then please check your error-logs - either in your hosting provider admin panel or in some /logs dir
+                                           // on your webspace.
+                                           // LimeSurvey developers: Set this to 3 to circumvent the restriction to remove the installation directory and full access to standard templates
+                                           // or to change the password. If you set it to 3 then PHP STRICT warnings will be shown additionally.
+
+$modrewrite         =   0;
+
+$uploadurl               = '/upload';
+$uploaddir               = __DIR__ . '/../../web/upload';
+
+// CMS Integration Settings
+// Set $embedded to true and specify the header and footer functions - for example if the survey is to be displayed embedded in a CMS
+$embedded = true;
+$embedded_inc = '';             // path to a php file to include
+$embedded_headerfunc = '';      // e.g. COM_siteHeader for geeklog
+$embedded_footerfunc = '';      // e.g. COM_siteFooter for geeklog
--- src/Tac/WeddingBundle/Controller/DefaultController.php
+++ src/Tac/WeddingBundle/Controller/DefaultController.php
@@ -14,12 +14,36 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+      $photos = array(
+
+        array(
+        'image'=>'/images/album/halong_bay.jpg',
+        'caption'=>'Halong Bay, Vietnam'
+        ),
+
+        array(
+        'image'=>'https://lh4.googleusercontent.com/-k7i32vbZtls/T0ljlLiejvI/AAAAAAAAAKo/6jgCfKx7O-Y/w532-h399-k/IMG_2771.JPG',
+        'caption'=>'Antigua, Guatemala'
+        ),
+        array(
+        'image'=>'https://lh5.googleusercontent.com/-1AKjkYA1www/T0lAi3Wss5I/AAAAAAAAJVA/b3uurMg5R80/s720/P1020598.JPG',
+        'caption'=>'Angkor Wat, Cambodia (2010)'
+        ),
+        array(
+        'image'=>'https://lh3.googleusercontent.com/-7rrj7HanuNg/T0lAkV_EKBI/AAAAAAAAJVY/5RQETnwaM80/s720/P1070266.JPG',
+        'caption'=>'Lake Geneva, Switzerland'
+        )
+      );
+
+
       $params = array(
         'bride'=>'Linda G. Greenberg',
         'groom'=>'Michael "Tac" Tacelosky',
         'bride_first'=>'Linda',
-        'groom_first'=>'Tac'
+        'groom_first'=>'Tac',
+        'photos'=>$photos,
       );
-      return $params;
+
+        return $params;
     }
 }
